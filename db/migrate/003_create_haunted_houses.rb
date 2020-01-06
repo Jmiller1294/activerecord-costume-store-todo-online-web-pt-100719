630c481422dce858bd790ec235b07baf3cb0546c
Justin Miller <jayman1294@gmail.com>
2020-01-06 09:03:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,5 @@
 # Create your haunted_houses migration here
 class HauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table : 
+    create_table :haunted_houses do |t|
+      t.string :
