f8dbbf4f83feb5f47750834d649b17a9d4908a90
Justin Miller <jayman1294@gmail.com>
2020-01-06 08:57:26 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume do |t|
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.integer :still_in_business
+      t.integer
+      
