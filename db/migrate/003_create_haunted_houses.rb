64a835daeabb9b78dc3d28abfa11d340a9aba2e4
Justin Miller <jayman1294@gmail.com>
2020-01-06 09:06:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,4 +9,4 @@ class HauntedHouses < ActiveRecord::Migration[4.2]
       t.string :family_friendly
       t.string :opening_date
       t.string :closing_date
-      t.string :long_description
+      t.string :description
