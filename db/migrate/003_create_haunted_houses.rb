de12565466662bdd2422f515afccc089e2e0c92d
Justin Miller <jayman1294@gmail.com>
2020-01-06 09:04:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,4 +2,10 @@
 class HauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
-      t.string :
+      t.string :name 
+      t.string :location   
+      t.string :theme
+      t.string :price
+      t.string 
+      t.string
+      t.string
