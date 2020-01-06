3b80c63a7d4ff26705b161f449a228de1717d67d
Justin Miller <jayman1294@gmail.com>
2020-01-06 09:09:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CostumeStores < ActiveRecord::Migration[4.2]
       t.string :location 
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.integer :still_in_business
+      t.boolean :still_in_business
       t.string :opening_time
       t.string :closing_time
     end
